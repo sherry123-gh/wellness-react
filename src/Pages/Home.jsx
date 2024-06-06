a5a76186e8ab0548dd7359d6f7014e9b62073318
KIGENYI SHERINA <sherinakigenyi60@gmail.com>
2024-06-06 15:45:38 +0300

    edit images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
+import img1 from "/src/images/nutriton.jpg"
+import img2 from "/src/images/healthy.jpg"
+import img3 from "/src/images/workout.jpg"
+
+
 
 
 const Home = () => {
@@ -51,7 +56,7 @@ const Home = () => {
                         </p>
                         <div className="service_grid">
                             <div className="service_card">
-                                <span><img src={"nutrition.jpg"} alt="" height="300"width="400" /></span>
+                                <span><img src={img1} alt="" height="300"width="400" /></span>
                                 <h4>NUTRITION PLANS</h4>
                                 <p>Embarking on a nutrition plan is like planting seeds for a healthier you! Just imagine your body as a 
                                     vibrant garden, with each meal and snack nurturing your health blooms. Your personalized nutrition plan is 
@@ -66,7 +71,7 @@ const Home = () => {
                     
                     
                     <div className="service_card">
-                        <span><img src={"src/images/healthy.jpg"} alt=""height="300"width="400"/></span>
+                        <span><img src={img2} alt=""height="300"width="400"/></span>
                         <h4>HEALTHY TIPS</h4>
                         <p>Let's dive into a treasure trove of adorable ways to boost your health! Picture yourself as a wellness 
                             explorer, discovering the secret paths to a happier, healthier life. Start your day with a giggle-filled 
@@ -81,7 +86,7 @@ const Home = () => {
                     </div>
 
                     <div className="service_card">
-                        <span><img src={"src/Images/workout.jpg"} alt=""height="300"width="400"/></span>
+                        <span><img src={img3} alt=""height="300"width="400"/></span>
                         <h4>WORKOUT ROUTINES</h4>
                         <p>Let's embark on a fitness journey that's as fun as a hopscotch game! When it comes to workouts, think
                             of yourself as a joyful athlete in training. Start with warm-up dances that make your heart skip with 
